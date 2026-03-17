33972fc46c30e1cd6c79ef0e189408bbc7fb1b68
fiwka <dropinclass1@gmail.com>
2026-03-17 23:47:18 +0500

    fix: web dependencies in analytics-service

--- analytics-service/build.gradle.kts
+++ analytics-service/build.gradle.kts
@@ -3,10 +3,14 @@ plugins {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     implementation("com.github.ben-manes.caffeine:caffeine")
     implementation("org.springframework.boot:spring-boot-starter-kafka")
     testImplementation("org.springframework.kafka:spring-kafka-test")
+    runtimeOnly(libs.postgresql)
 }
 
 dependencyManagement {
