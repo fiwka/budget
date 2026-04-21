b183b57bae974fee268b2ca6bdcda23c81f01db8
Константин <dropinclass1@gmail.com>
2026-04-22 00:01:18 +0500

    fix: kotlin deleting "is" prefix in data classes after compilation, try direct hint via @JsonProperty

--- data-service/src/main/kotlin/xyz/fiwka/budget/dataservice/infrastructure/dto/request/category/CategoryFieldsRequest.kt
+++ data-service/src/main/kotlin/xyz/fiwka/budget/dataservice/infrastructure/dto/request/category/CategoryFieldsRequest.kt
@@ -1,5 +1,6 @@
 package xyz.fiwka.budget.dataservice.infrastructure.dto.request.category
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Size
@@ -11,6 +12,8 @@ data class CategoryFieldsRequest(
     @field:NotBlank
     @field:Size(max = 255, min = 2)
     val name: String,
+    @get:JsonProperty("isConsumption")
+    @param:JsonProperty("isConsumption")
     val isConsumption: Boolean
 )
 
--- data-service/src/main/kotlin/xyz/fiwka/budget/dataservice/infrastructure/dto/response/category/CategoryResponse.kt
+++ data-service/src/main/kotlin/xyz/fiwka/budget/dataservice/infrastructure/dto/response/category/CategoryResponse.kt
@@ -1,11 +1,14 @@
 package xyz.fiwka.budget.dataservice.infrastructure.dto.response.category
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import java.util.UUID
 
 data class CategoryResponse(
     val id: UUID,
     val budgetId: UUID,
     val name: String,
+    @get:JsonProperty("isConsumption")
+    @param:JsonProperty("isConsumption")
     val isConsumption: Boolean
 )
 
