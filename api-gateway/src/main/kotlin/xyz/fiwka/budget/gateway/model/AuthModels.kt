2987bee4165da64b6d924ad02dfd8bf8581d534a
Константин <dropinclass1@gmail.com>
2026-04-22 22:26:11 +0500

    fix: redis serialization

--- api-gateway/src/main/kotlin/xyz/fiwka/budget/gateway/model/AuthModels.kt
+++ api-gateway/src/main/kotlin/xyz/fiwka/budget/gateway/model/AuthModels.kt
@@ -1,5 +1,7 @@
 package xyz.fiwka.budget.gateway.model
 
+import java.io.Serializable
+
 data class LoginRequest(
     val login: String,
     val password: String
@@ -19,7 +21,7 @@ data class AuthSessionTokens(
     val accessToken: String,
     val refreshToken: String,
     val tokenType: String
-)
+) : Serializable
 
 data class SessionStatusResponse(
     val authenticated: Boolean,
