85b16f4c8b5f562a86de9d8672fc7fa479ae3152
fiwka <dropinclass1@gmail.com>
2026-04-29 22:25:15 +0500

    fix: cors

--- api-gateway/src/main/kotlin/xyz/fiwka/budget/gateway/controller/AuthSessionController.kt
+++ api-gateway/src/main/kotlin/xyz/fiwka/budget/gateway/controller/AuthSessionController.kt
@@ -1,5 +1,6 @@
 package xyz.fiwka.budget.gateway.controller
 
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -13,6 +14,7 @@ import xyz.fiwka.budget.gateway.service.AuthSessionService
 
 @RestController
 @RequestMapping("/api/session")
+@CrossOrigin(origins = ["http://localhost:5173"], maxAge = 3600, allowCredentials = "true")
 class AuthSessionController(
     private val authSessionService: AuthSessionService
 ) {
