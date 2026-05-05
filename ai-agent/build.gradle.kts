45e7acccd386f0b49c5b1f3994a234b732bf1059
fiwka <dropinclass1@gmail.com>
2026-05-05 21:54:30 +0500

    fix: downgrade spring boot in ai agent

--- ai-agent/build.gradle.kts
+++ ai-agent/build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
-    id("buildsrc.convention.spring-boot")
+    id("buildsrc.convention.spring-boot3")
 }
 
 dependencies {
+    implementation(platform("org.springframework.boot:spring-boot-dependencies:3.5.14"))
     implementation(platform("org.springframework.ai:spring-ai-bom:1.1.5"))
     implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     implementation("org.springframework.cloud:spring-cloud-starter-loadbalancer")
